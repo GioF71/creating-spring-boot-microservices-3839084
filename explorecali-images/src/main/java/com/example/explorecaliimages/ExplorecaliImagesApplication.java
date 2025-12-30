38b946d5c2730732a7bd89c9ae68b746541c765a
Giovanni Fulco <giovanni.fulco@gmail.com>
2025-12-30 18:03:05 +0100

    Match solution ...

--- explorecali-images/src/main/java/com/example/explorecaliimages/ExplorecaliImagesApplication.java
+++ explorecali-images/src/main/java/com/example/explorecaliimages/ExplorecaliImagesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ImagesApplication {
+public class ExplorecaliImagesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ImagesApplication.class, args);
+		SpringApplication.run(ExplorecaliImagesApplication.class, args);
 	}
 
 }
